c8b6bdeaefc5640ee15536e3e4e0bf0272f94e5c
Heniloza <ozahenil73@gmail.com>
2026-04-29 14:24:33 +0530

    UI : Resolved issues and implemented sliders in brands and testimonil

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ currencySelect.addEventListener("change", function () {
     flagImg.src = "./assets/flags/uk.png";
   }
 });
+
+var brandSwiper = new Swiper(".brandSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var testimonialSwiper = new Swiper(".testimonialSwiper", {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
